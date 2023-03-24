f828f63c4f9f2ae6330f9fcc0017977b6476a1db
tomato <kaguratomato1@gmail.com>
2023-03-24 17:59:19 +0900

    画像の添付

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,26 @@
-Rails.application.routes.draw do 
-  root 'tops#index' 
-  get '/', to: 'tops#index' 
-  get '/will', to: 'pages#will' 
-  get '/can', to: 'pages#can' 
-  get '/must', to: 'pages#must' 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html 
-  end 
+# Rails.application.routes.draw do 
+#   get 'pages/will'
+
+#   get 'pages/can'
+
+#   get 'pages/must'
+
+#   get 'tops/index'
+
+#   root 'tops#index' 
+#   get '/', to: 'tops#index' 
+#   get '/will', to: 'pages#will' 
+#   get '/can', to: 'pages#can' 
+#   get '/must', to: 'pages#must' 
+#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html 
+#   end 
   
+  Rails.application.routes.draw do 
+    root 'tops#index' 
+    get '/', to: 'tops#index' 
+    get '/will', to: 'pages#will' 
+    get '/can', to: 'pages#can' 
+    get '/must', to: 'pages#must' 
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html 
+    end 
+    
